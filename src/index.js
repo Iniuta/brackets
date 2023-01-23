0d15d32e3fc0c029459ee7fee98513aaacaf021b
Iniuta <innazolotkomoe@gmail.com>
2023-01-23 19:04:49 +0100

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let objectConfig = new Map(bracketsConfig);
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].includes(objectConfig.values()) && stack.length === 0){
+      return false
+    } else if(str[i]===(objectConfig.get(stack[stack.length - 1]))) {
+        stack.pop();
+      } else {
+          stack.push(str[i]);
+      }
+  }
+  return stack.length === 0;
+};
